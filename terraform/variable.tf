71e0eaa0bc5dd903bb56a1be7597601a424a2cdf
shafiunp <phatanshafiun@gmail.com>
2023-02-20 21:00:09 +0400

    updated terraform file

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,5 @@
 variable "foo_ami" {
-  description = "foo ami value" # ami-06ee4e2261a4dc5c3
+  description = "foo ami value" #ami-0e742cca61fb65051
 }
 variable "foo_instance_type" {
   description = "foo instance type value" # t2.micro
