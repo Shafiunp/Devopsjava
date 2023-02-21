6bea171ffcc45023c7f0fb9ad49c504a5ab3cfd6
shafiunp <phatanshafiun@gmail.com>
2023-02-21 10:00:37 +0400

    updated terraformfile

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,3 +4,9 @@ output "foo_ami_output" {
 output "foo_tags_output" {
   value = aws_instance.foo1.tags
 }
+output "shafiun_ami_output" {
+  value = aws_instance.shafiun.ami
+}
+output "shafiun_instance_type_output" {
+  value = aws_instance.shafiun.instance_type
+}
